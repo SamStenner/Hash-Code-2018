d3d67ca90ebd4e0fd1637f1ab19b5e382e8a0401
Meown12 <maxpruestel@web.de>
2018-03-01 18:19:07 +0000

    some read stuff

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-print("Hello")
-print("Hello")
+time =  0
+rowNumber = None
+columnNumber = None
+vehicleNumber = None
+ridesNumber = None
+bonusFactor = None
+rideList = None
+totalTime = None
+map = None
+carPositionList = None
+
+def readFile(fileName):
+    file = open(fileName, 'r')
+    firstLine = True
+    for line in file:
+        elements = line.split(' ')
+        if firstLine:
+            firstLine = False
+            rowNumber = elements[0]
+            columnNumber = elements[1]
+            vehicleNumber = elements[2]
+            ridesNumber = elements[3]
+            bonusFactor = elements[4]
+            totalTime = elements[5]
+        else:
+            # calculate distance
+            # store distance in ride info in array
+            rideInfo = elements
+            rideInfo.append(None)# distance
+            rideList.append(rideInfo)
+
+        carPositionList = [[0,0] for  number in vehicleNumber]
+
+
+
+
