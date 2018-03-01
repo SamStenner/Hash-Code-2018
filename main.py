9a847c1c8d185c0cbced0696e675befa7cd2b008
SamStenner <sam.stenner@hotmail.co.uk>
2018-03-01 20:10:28 +0000

    Stufffffff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-time =  0
+time = 0
 rowNumber = None
 columnNumber = None
 vehicleNumber = None
@@ -16,31 +16,34 @@ def readFile(fileName):
         if firstLine:
             firstLine = False
             global rowNumber
-            rowNumber = elements[0]
+            rowNumber = int(elements[0])
             global columnNumber
-            columnNumber = elements[1]
+            columnNumber = int(elements[1])
             global vehicleNumber
-            vehicleNumber = elements[2]
+            vehicleNumber = int(elements[2])
             global ridesNumber
-            ridesNumber = elements[3]
+            ridesNumber = int(elements[3])
             global bonusFactor
-            bonusFactor = elements[4]
+            bonusFactor = int(elements[4])
             global totalTime
-            totalTime = elements[5]
+            totalTime = int(elements[5])
         else:
             elements[5] = elements[5].strip('\n')
+            for i in range(0, len(elements)):
+                elements[i] = int(elements[i])
             elements.append(False)
             elements.append(calc_dist(elements[0], elements[1], elements[2], elements[3]))
             rideInfo = elements
             rideList.append(rideInfo)
         global carList
-        carList = [[0, 0, False] for number in vehicleNumber]
+        carList = [[0, 0, False, None] for number in range(0, vehicleNumber)]
+    file.close()
 
 
-def calcWeightForCar(carPos, journey):
-    lateTime = journey[5]
-    distJourney = journey[6]
-    distToStart = calc_dist(carPos[0], carPos[1], journey[0], journey[1])
+def calc_car_weight(carX, carY, ride):
+    lateTime = int(ride[5])
+    distJourney = int(ride[6])
+    distToStart = calc_dist(carX, carY, ride[0], ride[1])
     return lateTime - time - distToStart - distJourney
 
 
@@ -49,12 +52,67 @@ def calc_dist(x1, y1, x2, y2):
     d_y = abs(int(y1) - int(y2))
     return d_x + d_y
 
+
+def simulate():
+    global time
+    global totalTime
+    time = 0
+    finished = False
+    for car in carList:
+        car[3] = select_journey(car)
+    while not finished:
+        for carIndex, car in enumerate(carList, 0):
+            if car[2] == False:
+                ride = rideList[car[3]]
+                if car[0] == ride[0] and car[1] == ride[1] and time >= ride[4]:
+                    car[2] = True
+                else:
+                    x, y = update_car_coords(car[0], car[1], ride[0], ride[1])
+                    car[0] = x
+                    car[1] = y
+            if car[2] == True:
+                current_ride = rideList[car[3]]
+                if car[0] == current_ride[2] == True and car[1] == current_ride[3]:
+                    car[2] = False
+                    rideList[car[3]] = None
+                    car[3] = select_journey(car)
+                else:
+                    car[0], car[2] = update_car_coords(car[0], car[1], current_ride[2], current_ride[3])
+        if time < totalTime:
+            time += 1
+        else:
+            finished = True
+    print("Simulation finished")
+
+
+def select_journey(car):
+    best_score = 99999999999999
+    best_ride_index = 0
+    for i in range(0, len(rideList)):
+        if rideList[i] is not None:
+            score = calc_car_weight(car[0], car[1], rideList[i])
+            if score < best_score:
+                best_ride_index = i
+                best_score = score
+    return best_ride_index
+
+
+def update_car_coords(x1, y1, x2, y2):
+    if x1 != x2:
+        if x1 > x2:
+            x1 = x1 - 1
+        else:
+            x1 = x1 + 1
+    else:
+        if y1 > y2:
+            y1 = y1 - 1
+        else:
+            y1 = y1 + 1
+    return x1, y1
+
 if __name__ == "__main__":
     readFile("a_example.in")
-    for i in rideList:
-        print(i)
-
-
+    simulate()
 
 
 
